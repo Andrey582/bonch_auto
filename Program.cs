6b19899f08ebb888e36ae5377f8c26361758321f
Andrey582 <gandrey0606@mail.ru>
2020-12-01 21:44:02 +0300

    шо это?

--- Program.cs
+++ Program.cs
@@ -1,13 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace HelloWorldd
+namespace ConsoleApp9
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.Write("Дима Пидор))))\nТы крип, крипочек");
+            Console.Write("Дима крип, крипочек");
         }
     }
 }
